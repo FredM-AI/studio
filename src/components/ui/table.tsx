b31bbd5cc799eb2ac14a895785773364abe77f3d
frederic marre <marrefre@gmail.com>
2025-06-06 15:59:48 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -7,7 +7,7 @@ const Table = React.forwardRef<
   HTMLTableElement,
   React.HTMLAttributes<HTMLTableElement>
 >(({ className, ...props }, ref) => (
-  <div className="relative w-full overflow-auto"> {/* Ensure this div handles scrolling */}
+  <div className="relative w-full overflow-auto">
     <table
       ref={ref}
       className={cn("w-full caption-bottom text-sm", className)}
@@ -55,16 +55,18 @@ TableFooter.displayName = "TableFooter"
 const TableRow = React.forwardRef<
   HTMLTableRowElement,
   React.HTMLAttributes<HTMLTableRowElement>
->(({ className, ...props }, ref) => (
+>(({ className, children, ...restProps }, ref) => (
   <tr
     ref={ref}
     className={cn(
       "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
       className
     )}
-    {...props}
-  />
-))
+    {...restProps}
+  >
+    {children}
+  </tr>
+));
 TableRow.displayName = "TableRow"
 
 const TableHead = React.forwardRef<
